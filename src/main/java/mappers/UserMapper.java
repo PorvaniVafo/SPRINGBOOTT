48bb38737fe014ce4118f5281846e0ba5e4ddc46
PorvaniVafo <PorvaniVafo>
2024-04-02 21:21:17 +0600

    ...

--- src/main/java/mappers/UserMapper.java
+++ src/main/java/mappers/UserMapper.java
@@ -3,11 +3,10 @@ package mappers;
 import response.UserResponseDTO;
 import reuest.UserRequestDTO;
 import entity.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.factory.Mappers;
 
 public interface UserMapper {
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     UserResponseDTO toDTO(User user);
     User toEntity(UserRequestDTO userRequestDTO);
