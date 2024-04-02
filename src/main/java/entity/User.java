39bec28ef5dedbee5fc124fabae3a8e066b7d43c
PorvaniVafo <PorvaniVafo>
2024-04-02 15:10:47 +0600

    entity

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,18 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+}
