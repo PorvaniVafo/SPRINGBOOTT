b01c8434027bcbfd64af144f896d09511994a64d
PorvaniVafo <PorvaniVafo>
2024-04-02 16:03:57 +0600

    ...

--- src/main/java/mappers/OrderMapper.java
+++ src/main/java/mappers/OrderMapper.java
@@ -3,11 +3,8 @@ package mappers;
 import response.OrderResponseDTO;
 import reuest.OrderRequestDTO;
 import entity.Order;
-import entity.Product;
-import entity.User;
 
 
-@Mapper(componentModel = "spring", uses = {User.class, Product.class})
 public interface OrderMapper {
 
     Order toEntity(OrderRequestDTO orderRequestDTO);
---
+++ src/test/java/User/UserServiceTest.java
@@ -0,0 +1,64 @@
+package User;
+
+import response.UserResponseDTO;
+import reuest.UserRequestDTO;
+import entity.User;
+import mappers.UserMapper;
+import repositories.UserRepository;
+import services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testCreateUser_Success() {
+        UserRequestDTO requestDTO = new UserRequestDTO();
+        requestDTO.setUsername("testUser");
+        requestDTO.setEmail("test@example.com");
+        requestDTO.setPassword("password");
+
+        User user = new User();
+        user.setId(1L);
+        user.setUsername(requestDTO.getUsername());
+        user.setEmail(requestDTO.getEmail());
+        user.setPassword(requestDTO.getPassword());
+
+        UserResponseDTO expectedResponse = new UserResponseDTO();
+        expectedResponse.setId(1L);
+        expectedResponse.setUsername(user.getUsername());
+        expectedResponse.setEmail(user.getEmail());
+
+        when(userRepository.existsByUsername(requestDTO.getUsername())).thenReturn(false);
+        when(userRepository.existsByEmail(requestDTO.getEmail())).thenReturn(false);
+        when(userMapper.toEntity(requestDTO)).thenReturn(user);
+        when(userRepository.save(user)).thenReturn(user);
+        when(userMapper.toDTO(user)).thenReturn(expectedResponse);
+
+        UserResponseDTO actualResponse = userService.create(requestDTO);
+
+        assertNotNull(actualResponse);
+        assertEquals(expectedResponse, actualResponse);
+    }
+}
