f8a9f9c893cc71ba905909d6b2e2cc829b701adf
PorvaniVafo <PorvaniVafo>
2024-04-04 17:15:15 +0600

    ...

--- src/main/java/springboot/mappers/OrderMapper.java
+++ src/main/java/springboot/mappers/OrderMapper.java
@@ -1,10 +1,11 @@
 package springboot.mappers;
 
+import org.mapstruct.Mapper;
 import springboot.response.OrderResponseDTO;
 import springboot.reuest.OrderRequestDTO;
 import springboot.entity.Order;
 
-
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
 
     Order toEntity(OrderRequestDTO orderRequestDTO);
--- src/main/java/springboot/mappers/UserMapper.java
+++ src/main/java/springboot/mappers/UserMapper.java
@@ -1,10 +1,11 @@
 package springboot.mappers;
 
+import org.mapstruct.Mapper;
 import springboot.entity.User;
 import springboot.response.UserResponseDTO;
 import springboot.reuest.UserRequestDTO;
 
-
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserResponseDTO toDTO(User user);
